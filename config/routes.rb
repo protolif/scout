236e0ccf900dfe344ba9fd84fae33c8ff2750e31
James Dunn <jamesldunnjr@gmail.com>
2021-10-22 19:03:57 -0400

    Created a welcome controller and home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'welcome/home'
   # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  root "welcome#home"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
